877f50e826daa249485d95055147ca0f56a8107e
mimicria <gmimicria@gmail.com>
2026-04-30 07:33:41 +0300

    Initial commit

---
+++ src/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+
+// ПРЕДУПРЕЖДЕНИЕ: В боевом коде не должно быть жестко прописанных ключей!
+std::string api_key = "AIzaSyAw1234567890-AbCdEfGhIjKlMnOpQrStU"; // Google API Key
+std::string db_password = "supersecretpassword123!";
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
